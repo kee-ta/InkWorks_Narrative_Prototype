8a5e86d95832b2f09f95dcd170d74892758091e9
Kitanae <kadokawa.kitanae@gmail.com>
2024-07-08 17:52:05 +0900

    InitialCommit
    =

---
+++ InkWorks_NarrativeProject/Assets/Dialogue/DemoScript.ink
@@ -0,0 +1,17 @@
+Hello there! #speaker:Demo NPC #portrait:dr_green_neutral #layout:left #audio:animal_crossing_mid
+-> main
+
+=== main ===
+How are you feeling today?
++ [Happy]
+    That makes me feel <color=\#F8FF30>happy</color> as well! #portrait:dr_green_happy
++ [Sad]
+    Oh, well that makes me <color=\#5B81FF>sad</color> too. #portrait:dr_green_sad
+    
+- Well, do you have any more questions? #speaker:Dr. Green #portrait:dr_green_neutral #layout:left #audio:animal_crossing_mid
+
++ [Yes]
+    -> main
++ [No]
+    Goodbye then!
+    -> END
---
+++ InkWorks_NarrativeProject/Assets/InkleDialogueSystem/Scripts/Dialogue/DialogueTrigger.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueTrigger : MonoBehaviour
+{
+    [Header("Ink JSON")]
+    [SerializeField] private TextAsset inkJSON;
+
+    private bool playerInRange;
+
+    private void Awake() 
+    {
+        playerInRange = false;
+    }
+
+    private void Update() 
+    {
+        if (Input.anyKeyDown && !DialogueManager.GetInstance().dialogueIsPlaying) 
+        {
+                DialogueManager.GetInstance().EnterDialogueMode(inkJSON);
+        }
+        else 
+        {
+
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collider) 
+    {
+        if (collider.gameObject.tag == "Player")
+        {
+            playerInRange = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collider) 
+    {
+        if (collider.gameObject.tag == "Player")
+        {
+            playerInRange = false;
+        }
+    }
+}
